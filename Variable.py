89464ad8647d032379045c8bb2477752e13a6d42
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-26 16:27:21 +0530

    varibales and basic data types-str,int

--- Variable.py
+++ Variable.py
@@ -1,3 +1,34 @@
 # Variable and Data types in python
 # Exercise-1 print hell world
 print('Hello World!')
+
+# Exercise-2 store your name and age and print it
+name='Nikhil'
+age=21
+print(f'My name is {name}\nI am {age} year old')
+
+#Exercise-3 Add,subtract,multiply two numbers
+num_1=12
+num_2=9
+print(f"addition of two number is:{num_1+num_2}")
+print(f"subtraction of two number is:{num_1-num_2}")
+print(f"multiplication of two number is:{num_1*num_2}")
+
+#Exercsie-4 find the remainder of two number
+num_3=8
+num_4=2
+print(f"Remainder of two number is:{num_3%num_4}")
+
+#Exercise-5 convert string to integer and vice verse
+str_num='121'
+print(type(str_num))
+print('Converting to integer')
+int_num=int(str_num)
+print(type(int_num))
+
+#Exercise-6 create a student information program
+studentName='Nikhil'
+studentAge=21
+courseEnrolled='Software Programming and Development'
+print('Student Information:')
+print(f'StudentName is:{studentName}\nStudentAge is:{studentAge}\nCourse Enrolled:{courseEnrolled}')
